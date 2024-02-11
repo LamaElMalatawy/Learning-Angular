75f8fcb4742efc56de48f4a7a68ef14007d68a62
LamaElMalatawy <lamaelmalatawy@gmail.com>
2024-02-11 15:05:45 +0200

    template ref

--- templateReference/src/app/demo/demo.component.ts
+++ templateReference/src/app/demo/demo.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-demo',
+  selector: 'demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
 export class DemoComponent {
+
+
+  sayHello(inputElement: HTMLInputElement){
+    alert('Hello '+inputElement.value);
+  }
 
 }
